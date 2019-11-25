7b53446ec47db66cc4ffd380cc22dd54512d898f
Byron Torres <btorres3@hawk.iit.edu>
2019-11-24 18:14:34 -0600

    (mostly) completed code and testbench for control unit

---
+++ src/control.vhd
@@ -0,0 +1,109 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Signals and their description:
+-- PCWriteCond: Load conditionally branch address into PC if Zero = 1
+-- PCWrite: Load unconditionally next instruction address into PC
+-- IorD: Read data from either PC address or LW address
+-- MemRead and MemWrite: Read or write for memory
+-- MemtoReg: Write data from either Memory Data Register (MDR) or ALUoutput (R-type)
+-- IRWrite: Controls whether to write to instruction register
+-- ALUSrcA: Selects either the PC or RegFile(IR[25-21]) data for one of the ALU's operands
+-- RegWrite: Controls whether to write to register file
+-- RegDst: Specifies which register to write to, either IR[20-16] from LW or IR[15-11] from R-Type Instructions
+-- PCSource: Selects one of the three possible choices for new PC (00: PC + 4) (01: ALUout from beq) (10: j-type addr)
+-- ALUSrcB: Selects one of four possible choices for the second ALU operand:
+    -- 00: RegFile(IR[20-16]) for R-Type
+    -- 01: 4 for PC = PC + 4
+    -- 10: sign_extend(IR[15-0]) for LW
+    -- 11: shift_left2(sign_extend(IR[15-0])) for beq
+-- ALUOp: Selects which ALU operation to perform
+
+entity control is
+    port(opcode: in std_logic_vector(5 downto 0);
+         PCWriteCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, ALUSrcA, RegWrite, RegDst: out std_logic;
+         PCSource, ALUSrcB, ALUOp: out std_logic_vector(1 downto 0));
+end control; 
+
+architecture behavioral of control is
+begin
+    process(opcode)
+    begin
+    case opcode is
+        when "100011" => -- lw
+            PCWriteCond <= '0';
+            PCWrite <= '0';
+            IorD <= '0';
+            MemRead <= '1';
+            MemWrite <= '0';
+            MemtoReg <= '1';
+            IRWrite <= '1';
+            ALUSrcA <= '1';
+            RegWrite <= '1';
+            RegDst <= '0';
+            PCSource <= "00";
+            ALUSrcB <= "10";
+            ALUOp <= "00";
+        when "000000" => -- add
+            PCWriteCond <= '0';
+            PCWrite <= '0';
+            IorD <= '0';
+            MemRead <= '0';
+            MemWrite <= '0';
+            MemtoReg <= '0';
+            IRWrite <= '1';
+            ALUSrcA <= '1';
+            RegWrite <= '1';
+            RegDst <= '1';
+            PCSource <= "00";
+            ALUSrcB <= "00";
+            ALUOp <= "10";
+        when "000010" => -- j
+            PCWriteCond <= '0';
+            PCWrite <= '1';
+            IorD <= '0';
+            MemRead <= '0';
+            MemWrite <= '0';
+            MemtoReg <= '0';
+            IRWrite <= '1';
+            ALUSrcA <= '0';
+            RegWrite <= '0';
+            RegDst <= '0';
+            PCSource <= "10";
+            ALUSrcB <= "00";
+            ALUOp <= "10";
+        when "000100" => -- beq
+            PCWriteCond <= '1';
+            PCWrite <= '0';
+            IorD <= '0';
+            MemRead <= '0';
+            MemWrite <= '0';
+            MemtoReg <= '0';
+            IRWrite <= '0';
+            ALUSrcA <= '0';
+            RegWrite <= '0';
+            RegDst <= '0';
+            PCSource <= "01";
+            ALUSrcB <= "11";
+            ALUOp <= "01";
+        when "001000" => -- addi
+            PCWriteCond <= '0';
+            PCWrite <= '1';
+            IorD <= '0';
+            MemRead <= '0';
+            MemWrite <= '0';
+            MemtoReg <= '0';
+            IRWrite <= '0';
+            ALUSrcA <= '0';
+            RegWrite <= '0';
+            RegDst <= '0';
+            PCSource <= "01";
+            ALUSrcB <= "11";
+            ALUOp <= "01";
+        when others =>
+    end case;
+end process;
+end behavioral;
+
+
---
+++ tb/control_tb.vhd
@@ -0,0 +1,67 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity control_tb is
+end control_tb;
+
+architecture behavioral of control_tb is 
+    signal op                                            : std_logic_vector(5 downto 0);
+    signal pWC, pW, I, mR, mW, MtR, IW, aluA, regW, regD : std_logic;
+    signal pcS, aluB, aluOP                              : std_logic_vector(1 downto 0);
+
+    -- The component we are testing is the control unit,
+    -- so we declare that component here.
+    component control is
+        port(opcode: in std_logic_vector(5 downto 0);
+             PCWriteCond, PCWrite, IorD, MemRead, MemWrite, MemtoReg, IRWrite, ALUSrcA, RegWrite, RegDst: out std_logic;
+             PCSource, ALUSrcB, ALUOp: out std_logic_vector(1 downto 0));
+    end component control;
+
+begin
+
+    -- 'uut' stands for unit under test
+    -- And the unit we are testing is the
+    -- control unit.
+    uut : control
+
+    -- As we did for the full-adder project,
+    -- we must map each input and output of the control unit
+    -- to the signals we declared eariler. 
+    port map (
+        opcode => op,
+        PCWriteCond => pWC,
+        PCWrite => pW,
+        IorD => I,
+        MemRead => mR,
+        MemWrite => mW,
+        MemtoReg => MtR,
+        IRWrite => IW,
+        ALUSrcA => aluA,
+        RegWrite => regW,
+        regDst => regD,
+        PCSource => pcS,
+        ALUSrcB => aluB,
+        ALUOp => aluOP
+    );
+
+    -- The test for the control unit is very simple,
+    -- as all the tests just test each opcode we must implement.
+    -- And we check if the right control signals are asserted through
+    -- the waveform viewer.
+    process is
+    begin
+        op <= "100011";
+        wait for 1 ns;
+        op <= "000000";
+        wait for 1 ns;
+        op <= "000010";
+        wait for 1 ns;
+        op <= "000100";
+        wait for 1 ns;
+        op <= "001000";
+        wait for 1 ns;
+    end process;
+end behavioral;
+
+
+
