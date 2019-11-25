526787d4854d38a34506d868a5df8ccedab12a73
Byron Torres <btorres3@hawk.iit.edu>
2019-11-25 17:56:54 -0600

    clocked second shift left unit

--- src/shiftleft2.vhd
+++ src/shiftleft2.vhd
@@ -5,10 +5,16 @@ use ieee.std_logic_unsigned.all;
 
 entity shiftleft2 is
     port(input: in std_logic_vector(31 downto 0);
+         clk: in std_logic;
          output: out std_logic_vector(31 downto 0));
 end shiftleft2;
 
 architecture behavioral of shiftleft2 is
 begin
-    output <= std_logic_vector(shift_left(unsigned(input), 2));
+    shift: process(clk) is
+    begin
+        if rising_edge(clk) then
+            output <= std_logic_vector(shift_left(unsigned(input), 2));
+        end if;
+    end process;
 end behavioral;
--- tb/shiftleft2_tb.vhd
+++ tb/shiftleft2_tb.vhd
@@ -6,10 +6,12 @@ end shiftleft2_tb;
 
 architecture behavioral of shiftleft2_tb is
     signal x  : std_logic_vector(31 downto 0);
+    signal ck : std_logic;
     signal y  : std_logic_vector(31 downto 0);
 
     component shiftleft2 is
         port(input: in std_logic_vector(31 downto 0);
+             clk: in std_logic;
              output: out std_logic_vector(31 downto 0));
     end component shiftleft2;
 
@@ -19,19 +21,29 @@ begin
 
     port map (
         input  => x,
+        clk    => ck,
         output => y
     );
 
+    gen_clk : process
+    begin
+        ck <= '0';
+        wait for 1 ns;
+        ck <= '1';
+        wait for 1 ns;
+    end process;
+
     process is
     begin
+        wait for 1 ns;
         x <= x"10000001";
-        wait for 1 ns;
+        wait for 2 ns;
         x <= x"DEADBEEF";
-        wait for 1 ns;
+        wait for 2 ns;
         x <= x"ABADBABE";
-        wait for 1 ns;
+        wait for 2 ns;
         x <= x"A5A5A5A5";
-        wait for 1 ns;
+        wait for 2 ns;
         x <= x"29696969";
         wait for 1 ns;
     end process;
