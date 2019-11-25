a4fb97b7dfb83b842a6af24561669125e3146b71
Byron Torres <btorres3@hawk.iit.edu>
2019-11-25 17:52:14 -0600

    clocked shift left unit

--- src/shiftleft1.vhd
+++ src/shiftleft1.vhd
@@ -5,12 +5,16 @@ use ieee.std_logic_unsigned.all;
 
 entity shiftleft1 is
     port(input: in std_logic_vector(25 downto 0);
+         clk: in std_logic;
          output: out std_logic_vector(27 downto 0));
 end shiftleft1;
 
 architecture behavioral of shiftleft1 is
-    signal temp : std_logic_vector(27 downto 0);
 begin
-    temp   <= std_logic_vector(resize(signed(input), output'length));
-    output <= std_logic_vector(shift_left(unsigned(temp), 2));
+    shift: process(clk) is
+    begin
+        if rising_edge(clk) then
+            output <= std_logic_vector(shift_left(unsigned(resize(signed(input), output'length)), 2));
+        end if;
+    end process;
 end behavioral;
--- tb/shiftleft1_tb.vhd
+++ tb/shiftleft1_tb.vhd
@@ -6,10 +6,12 @@ end shiftleft1_tb;
 
 architecture behavioral of shiftleft1_tb is
     signal x  : std_logic_vector(25 downto 0);
+    signal ck : std_logic;
     signal y  : std_logic_vector(27 downto 0);
 
     component shiftleft1 is
         port(input: in std_logic_vector(25 downto 0);
+             clk: in std_logic;
              output: out std_logic_vector(27 downto 0));
     end component shiftleft1;
 
@@ -19,20 +21,30 @@ begin
 
     port map (
         input  => x,
+        clk    => ck,
         output => y
     );
 
+    gen_clk : process
+    begin
+        ck <= '0';
+        wait for 2 ns;
+        ck <= '1';
+        wait for 2 ns;
+    end process;
+
     process is
     begin
+        wait for 2 ns;
         x <= "10000000000000000000000000";
-        wait for 1 ns;
+        wait for 4 ns;
         x <= "00000000000000000000000000";
-        wait for 1 ns;
+        wait for 4 ns;
         x <= "11111111111111111111111111";
-        wait for 1 ns;
+        wait for 4 ns;
         x <= "01010101010101010101010101";
-        wait for 1 ns;
+        wait for 4 ns;
         x <= "10101010101010101010101010";
-        wait for 1 ns;
+        wait for 2 ns;
     end process;
 end behavioral;
