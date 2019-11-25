9dc12903fed5154e24f8fa3e774601700ce5e684
iuwizeyimana <iuwizeyimana@hawk.iit.edu>
2019-11-25 00:58:58 -0600

    Alu changes

--- src/alu.vhd
+++ src/alu.vhd
@@ -2,6 +2,7 @@
 --our input to the ALU would be the 4 bit ALU control signal our input A and B and Carryin
 -- for the 4 bit ALU signal, the most significant bit will be used as a select for A_invert in the A_invert Mux, the next bit as the select for the B_invert Mux and the last two to select the operation at hand
 --we will first make the adder, and and or entities then use those to create the ALU entity
+
 library ieee;
 use ieee.std_logic_1164.all;
 -- 1 bit full adder
@@ -39,14 +40,15 @@ end one_bit_or;
 -- 1 bit or architecture
 architecture or_behav  of one_bit_or is 
 begin
-	or_out <= a and  b;
+	or_out <= a or  b;
 end or_behav;
 
 --now we make the 1 bit ALU entity
+
 library ieee;
 use ieee.std_logic_1164.all;
-entity one_bit_ALU is port ( ALU_control : in std_logic_vector(3 downto 0);
-			a,b, CarryIn: in std_logic;
+entity one_bit_ALU is port( ALU_control : in std_logic_vector(3 downto 0);
+			a,b, CarryIn, clk: in std_logic;
 			CarryOut, Result: out std_logic);
 end one_bit_ALU;
 --our control signal comes in as 4 bit but needs to be split 
@@ -54,9 +56,9 @@ end one_bit_ALU;
 architecture ALU_1_behav of one_bit_ALU is
 	--let us declare internal signals
 	signal a_int, b_int: std_logic;
-	signal and_result, or_result, adder_result, set_result: std_logic;
+	signal and_result, or_result, adder_result, set_result, co : std_logic;
 	component FA
-		port(a, b, c: in std_logic;
+		port(a, b, ci: in std_logic;
 		     s, co: out std_logic);
 	end component;
 
@@ -70,44 +72,50 @@ architecture ALU_1_behav of one_bit_ALU is
 		     or_out: out std_logic);
 	end component;
 begin 
+    process(ALU_control, CarryIn) is
+	begin 
 	-- invert the two inputs in case they need to be inverted
-	if ALU_control(3) then
+    if ALU_control(3) = '1' then
 		 a_int <= not a;
 	else 
 		a_int <= a;
 	end if;
 	
-	if ALU_control(2) then
+	if ALU_control(2) = '1' then
 		 b_int <= not b;
 	else 
 		b_int <= b;
 	end if;
+	end process;
 	
-
-	FA0: FA port map(a_int, b_int, CarryIn, adder_result, CarryOut);
-	AND0: one_bit_and port map(a_int, b_int,and_result);
-	OR0: one_bit_or port map(a_int, b_int, or_result);
+	FA0: FA port map(a => a_int, b => b_int, ci => CarryIn, s => adder_result, co => co);
+	AND0: one_bit_and port map(a => a_int, b => b_int, and_out => and_result);
+	OR0: one_bit_or port map(a => a_int,b => b_int, or_out => or_result);
 	
-	if (adder_result > 0) then 
-			set_result <= 1; --set on less than, b<a
-	else 
-			set_result <= 0;
+	
+	process(adder_result, and_result, or_result, clk) is
+	begin
+	
+	if rising_edge(clk) then 
+     --   set_result <= adder_result; 	
+	case ALU_control(1 downto 0) is
+		when "00" => Result <= and_result; CarryOut <= '0';
+		when "01" => Result <= or_result; CarryOut <= '0';
+		when "10" => Result <= adder_result; CarryOut <= co;
+		when "11" => Result <= adder_result; CarryOut <= '0';
+		when others =>  Result <= '0'; CarryOut <= '0';
+	end case;
 	end if;
-	-- the last 2 bits of the ALU control are used in the mux (switch case) to select the operation 
-	case ALU_control(1:0) is
-		when "00" => Result <= and_result;
-		when "01" => Result <= or_result;
-		when "10" => Result <= add_result;
-		when "11" => Result <= set_result;
-		when others => Result <= 0;
+	end process;
+	
+	
 end ALU_1_behav;
-
 -- 4 bit ALU
 library ieee;
 use ieee.std_logic_1164.all;
 entity four_bit_ALU is
 	port ( ALU_control, a,b: in std_logic_vector (3 downto 0);
-		CarryIn: in std_logic;
+		CarryIn, clk: in std_logic;
 		CarryOut: out std_logic;
 		Result: out std_logic_vector(3 downto 0));
 end four_bit_ALU;
@@ -116,14 +124,14 @@ architecture four_bit_ALU_behav of four_bit_ALU is
 	signal c: std_logic_vector(2 downto 0); -- internal ripple carries
 	component one_bit_ALU
 		port(ALU_control: in std_logic_vector (3 downto 0);
-		     a, b, CarryIn: in std_logic;
+		     a, b, CarryIn, clk: in std_logic;
 		     CarryOut, Result: out std_logic);
 	end component;
 	begin
-		one_bit_ALU0: one_bit_ALU port map(ALU_control, a(0), b(0), CarryIn, c(0), Result(0));
-		one_bit_ALU1: one_bit_ALU port map(ALU_control, a(1), b(1), c(0), c(1), Result(1));
-		one_bit_ALU2: one_bit_ALU port map(ALU_control, a(2), b(2), c(1), c(2), Result(2));
-		one_bit_ALU3: one_bit_ALU port map(ALU_control, a(3), b(3), c(2), CarryOut, Result(3));
+		one_bit_ALU0: one_bit_ALU port map(ALU_control => ALU_control, a => a(0),b => b(0), CarryIn => CarryIn, clk => clk, CarryOut => c(0), Result => Result(0));
+		one_bit_ALU1: one_bit_ALU port map(ALU_control => ALU_control, a => a(1),b =>  b(1),CarryIn => c(0), clk => clk, CarryOut => c(1),Result => Result(1));
+		one_bit_ALU2: one_bit_ALU port map(ALU_control => ALU_control, a => a(2),b => b(2),CarryIn => c(1), clk => clk, CarryOut => c(2),Result => Result(2));
+		one_bit_ALU3: one_bit_ALU port map(ALU_control => ALU_control, a => a(3),b => b(3),CarryIn => c(2), clk => clk, CarryOut => CarryOut,Result => Result(3));
 end four_bit_ALU_behav;
 
 -- 32 bit ALU	
@@ -132,7 +140,7 @@ use ieee.std_logic_1164.all;
 entity thirtyTwo_bit_ALU is
 	port ( ALU_control: in std_logic_vector(3 downto 0);
 		 a,b: in std_logic_vector (31 downto 0);
-	--	CarryIn: in std_logic; we don't need a CarryIn input for the 32 bit ALU
+		clk : in std_logic;  --we don't need a CarryIn input for the 32 bit ALU
 		CarryOut: out std_logic;
 		Result: out std_logic_vector(31 downto 0));
 end thirtyTwo_bit_ALU;
@@ -142,27 +150,33 @@ architecture thirtyTwo_bit_ALU_behav of thirtyTwo_bit_ALU is
 
 	component four_bit_ALU
 		port ( ALU_control, a,b: in std_logic_vector (3 downto 0);
-			CarryIn: in std_logic;
+			CarryIn, clk: in std_logic;
 			CarryOut: out std_logic;
 			Result: out std_logic_vector(3 downto 0));
 	end component;
 begin
+    process(ALU_control) is
+    begin
 	if (ALU_control = X"6") then --then it's a subtraction
 --we will need to make the carryin 1 for the 2's complement as it will be b' plus 1
-		c(0) <= 1;
+		c(0) <= '1';
 	else 
-		c(0) <= 0;
+		c(0) <= '0';
 	end if;
+	end process;
 	
 	--now time for the 32 bit ALU using 4 bit ALU components
-	four0: four_bit_ALU(ALU_control, a(3 downto 0), b(3 downto 0), c(0), c(1), Result(3 downto 0));
-	four1: four_bit_ALU(ALU_control, a(7 downto 4), b(7 downto 4), c(1), c(2), Result(7 downto 4));
-	four2: four_bit_ALU(ALU_control, a(11 downto 8), b(11 downto 8), c(2), c(3), Result(11 downto 8));
-	four3: four_bit_ALU(ALU_control, a(15 downto 12), b(15 downto 12), c(3), c(4), Result(15 downto 12));
-	four4: four_bit_ALU(ALU_control, a(19 downto 16), b(19 downto 16), c(4), c(5), Result(19 downto 16));
-	four5: four_bit_ALU(ALU_control, a(23 downto 20), b(23 downto 20), c(5), c(6), Result(23 downto 20));
-	four6: four_bit_ALU(ALU_control, a(27 downto 24), b(27 downto 24), c(6), c(7), Result(27 downto 24));
-	four7: four_bit_ALU(ALU_control, a(31 downto 28), b(31 downto 28), c(7), CarryOut, Result(31 downto 28));
+	four0: four_bit_ALU port map(ALU_control => ALU_control,a => a(3 downto 0),b =>  b(3 downto 0),CarryIn =>  c(0), clk => clk, CarryOut => c(1), Result => Result(3 downto 0));
+	four1: four_bit_ALU port map(ALU_control => ALU_control,a => a(7 downto 4),b => b(7 downto 4),CarryIn => c(1),clk => clk, CarryOut => c(2),Result =>  Result(7 downto 4));
+	four2: four_bit_ALU port map (ALU_control => ALU_control,a => a(11 downto 8),b => b(11 downto 8),CarryIn => c(2),clk => clk, CarryOut => c(3), Result => Result(11 downto 8));
+	four3: four_bit_ALU port map(ALU_control => ALU_control,a => a(15 downto 12),b => b(15 downto 12),CarryIn => c(3),clk => clk, CarryOut => c(4),Result => Result(15 downto 12));
+	four4: four_bit_ALU port map(ALU_control => ALU_control,a => a(19 downto 16),b => b(19 downto 16),CarryIn => c(4),clk => clk, CarryOut => c(5),Result => Result(19 downto 16));
+	four5: four_bit_ALU port map (ALU_control => ALU_control,a => a(23 downto 20),b => b(23 downto 20),CarryIn => c(5),clk => clk, CarryOut => c(6),Result => Result(23 downto 20));
+	four6: four_bit_ALU port map(ALU_control => ALU_control,a => a(27 downto 24),b => b(27 downto 24),CarryIn => c(6), clk => clk, CarryOut => c(7),Result => Result(27 downto 24));
+	four7: four_bit_ALU port map(ALU_control => ALU_control,a => a(31 downto 28),b => b(31 downto 28),CarryIn => c(7),clk => clk, CarryOut => CarryOut, Result => Result(31 downto 28));
+	
+
 
 end thirtyTwo_bit_ALU_behav;
 	
+
--- tb/alu_1_tb.vhd
+++ tb/alu_1_tb.vhd
@@ -7,53 +7,132 @@ end one_bit_ALU_tb;
 architecture one_bit_ALU_behav of one_bit_ALU_tb is 
 	component one_bit_ALU
 		port(ALU_control: in std_logic_vector(3 downto 0);
-			a,b, CarryIn: in std_logic;
+			a,b, CarryIn, clk: in std_logic;
 			CarryOut, Result: out std_logic);
 	end component;
 
 	signal ALU_control: std_logic_vector(3 downto 0);
-	signal a,b,CarryIn, CarryOut, Result: std_logic;
+	signal a,b,CarryIn, clk, CarryOut, Result: std_logic;
 begin
 		-- unit under test
-	UUT: one_bit_ALU port map( ALU_control => ALU_control, a=>a, b=>b, CarryIn=>CarryIn, CarryOut=>CarryOut, Result=>Result);
+	UUT: one_bit_ALU port map( ALU_control => ALU_control, a=>a, b=>b, CarryIn=>CarryIn, clk => clk,  CarryOut=>CarryOut, Result=>Result);
+
+    gen_clk: process 
+    begin
+        clk <= '0';
+        wait for 5ps; 
+        clk <= '1';
+        wait for 5ps;
+    end process;
+	process
+	begin
+		ALU_control <= X"0"; --AND
+		a <= '1';
+		b <= '0';
+		CarryIn <= '0';
+		wait for 10ps;
+		
+		ALU_control <= X"1"; -- OR
+		a <= '0';
+		b <= '1';
+		CarryIn <= '0';
+		wait for 10ps;
+		
+		ALU_control <= X"2"; -- ADD
+		a <= '1';
+		b <= '0';
+		CarryIn <= '0';
+		wait for 10ps;
+
+		ALU_control <= X"6"; -- subtract
+		a <= '1';
+		b <= '0';
+		CarryIn <= '0';
+		wait for 10ps;
+
+		ALU_control <= X"7"; --set on less than
+		a <= '0';
+		b <= '0';
+		CarryIn <= '0';
+		wait for 10ps;
+
+		ALU_control <= X"C"; --NOR
+		a <= '0';
+		b <= '0';
+		CarryIn <= '0';
+		wait for 10ps;
+
+	end process;
+end one_bit_ALU_behav;
+
+-- 4 bit ALU TB
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity four_bit_ALU_tb is
+end four_bit_ALU_tb;
+
+architecture four_bit_ALU_behav of four_bit_ALU_tb is 
+	component four_bit_ALU
+		port(ALU_control, a, b: in std_logic_vector(3 downto 0);
+			CarryIn, clk: in std_logic;
+			CarryOut: out std_logic;
+			Result: out std_logic_vector(3 downto 0) );
+	end component;
+
+	signal ALU_control, a, b, Result: std_logic_vector(3 downto 0);
+	signal CarryIn, clk, CarryOut : std_logic;
+begin
+		-- unit under test
+	UUT: four_bit_ALU port map( ALU_control => ALU_control, a=>a, b=>b, CarryIn=>CarryIn,clk => clk, CarryOut=>CarryOut, Result=>Result);
+    
+    gen_clk: process 
+    begin
+        clk <= '0';
+        wait for 5ps; 
+        clk <= '1';
+        wait for 5ps;
+    end process;
 
 	process
 	begin
 		ALU_control <= X"0"; --AND
-		a <= 1;
-		b <= 1;
-		CarryIn <= 0;
+		a <= "0110";
+		b <= "1100";
+		CarryIn <= '0';
 		wait for 10ps;
 		
-		ALU_control <= X"1"; -- OR
-		a <= 1;
-		b <= 1;
-		CarryIn <= 0;
+		ALU_control <= X"1"; -- or
+		a <= "0110";
+		b <= "1100";
+		CarryIn <= '0';
 		wait for 10ps;
 		
-		ALU_control <= X"2"; -- ADD
-		a <= 1;
-		b <= 1;
-		CarryIn <= 0;
+		ALU_control <= X"2";  -- add
+		a <= "0110";
+		b <= "1100";
+		CarryIn <= '0';
 		wait for 10ps;
-
-		ALU_control <= X"6"; -- subtract
-		a <= 1;
-		b <= 1;
-		CarryIn <= 0;
+		
+		ALU_control <= X"6"; --sub
+		a <= "0110";
+		b <= "1100";
+		CarryIn <= '0';
 		wait for 10ps;
-
-		ALU_control <= X"7"; --set on less than
-		a <= 1;
-		b <= 1;
-		CarryIn <= 0;
+		
+		ALU_control <= X"7"; --slt
+		a <= "0110";
+		b <= "1100";
+		CarryIn <= '0';
 		wait for 10ps;
-
+		
 		ALU_control <= X"C"; --NOR
-		a <= 1;
-		b <= 1;
-		CarryIn <= 0;
+		a <= "0110";
+		b <= "1100";
+		CarryIn <= '0';
 		wait for 10ps;
-
+		
 	end process;
-end one_bit_ALU_behav 
+end four_bit_ALU_behav;
+
