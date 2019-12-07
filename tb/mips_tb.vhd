95d27eca293c117f742b704ebbfb3ea72615fcce
Byron Torres <btorres3@hawk.iit.edu>
2019-12-07 03:13:26 -0600

    changed starting pc and timing

--- tb/mips_tb.vhd
+++ tb/mips_tb.vhd
@@ -11,11 +11,11 @@ architecture behavioral of mips_tb is
     -- The component we are testing is the 2-input multiplexer,
     -- so we declare that component here.
     component mips is
-        port(clk, stall 	: in std_logic;
-	     pc_init		: in std_logic_vector(31 downto 0);
-	     pc_sel, mux_sel 	: in std_logic;
-	     mem_read, mem_write: in std_logic; 
-             ALUout		: out std_logic_vector(31 downto 0));
+        port(clk, stall 	     : in std_logic;
+	         pc_init		     : in std_logic_vector(31 downto 0);
+	         pc_sel, mux_sel 	 : in std_logic;
+	         mem_read, mem_write : in std_logic; 
+             ALUout		         : out std_logic_vector(31 downto 0));
     end component mips;
 
 begin
@@ -23,14 +23,14 @@ begin
     uut : mips
 
     port map (
-        clk    => clk,
+    clk    => clk,
 	stall  => stall,
 	pc_init => pc_init,
 	pc_sel => pc_sel,
 	mux_sel => mux_sel,
 	mem_read => mem_read,
 	mem_write => mem_write,
-        ALUout => ALUout
+    ALUout => ALUout
     );
 
     gen_clk : process
@@ -43,15 +43,15 @@ begin
     process
     begin
 	wait for 1 ns;
-        stall 	  <= '1';
-	pc_init   <= x"00000004";
-	pc_sel    <='1';
+    stall 	  <= '1';
+	pc_init   <= x"00000000";
+	pc_sel    <= '1';
 	mux_sel	  <= '0';
 	mem_read  <= '1';
 	mem_write <= '0';
-	wait for 50 ns;
+	wait for 10 ns;
 	stall 	  <= '0';
-	wait for 150 ns;
+    wait for 989 ns;
     end process;
 
 end behavioral;
